5fc9c679c13a74df296ae2fc7ec28917ce5e7b99
Łukasz Wójcik <lukaserex@gmail.com>
2020-08-19 16:42:02 +0200

    Added: Second pieChart in StatisticsFragment

--- app/src/main/java/com/example/scheduledfridge/ui/statistics/StatisticsFragment.kt
+++ app/src/main/java/com/example/scheduledfridge/ui/statistics/StatisticsFragment.kt
@@ -9,16 +9,17 @@ import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import com.example.scheduledfridge.R
 import com.example.scheduledfridge.database.Statistic
+import com.example.scheduledfridge.ui.history.HistoryViewModel
 import com.github.mikephil.charting.components.Legend
-import com.github.mikephil.charting.data.PieData
-import com.github.mikephil.charting.data.PieDataSet
-import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.data.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 import kotlinx.android.synthetic.main.fragment_statistics.*
 
 
 class StatisticsFragment : Fragment() {
     private val statisticsViewModel: StatisticsViewModel by activityViewModels()
+    private val  historyViewModel: HistoryViewModel by activityViewModels()
+    private val historyTypesAddedProducts = ArrayList<String>()
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
@@ -31,23 +32,72 @@ class StatisticsFragment : Fragment() {
         requireActivity().appBar_layout.elevation = 8F
         super.onViewCreated(view, savedInstanceState)
         statisticsViewModel.allStatistic.observe(viewLifecycleOwner, Observer {
-            setUpPieChart(it)
+            setUpDeletedPieChart(it)
+        })
+        historyViewModel.allHistory.observe(viewLifecycleOwner, Observer { it ->
+            it.forEach { historyProduct ->
+                if(historyProduct.action=="Added"){
+                    historyTypesAddedProducts.add(historyProduct.type)
+                }
+            }
+
+            setUpAddedPieChart()
 
         })
+
+
+
+
     }
 
-    private fun setUpPieChart(it: List<Statistic>) {
-        var eatenCount = 0.00
-        it.forEach { statistic ->
-            if (statistic.eaten) {
-                eatenCount++
-            }
+    private fun setUpAddedPieChart() {
+        val addedStatistics = ArrayList<PieEntry>()
+        val typesList = resources.getStringArray(R.array.types)
+        typesList.forEach {
+            addedStatistics.add(PieEntry(countStringInHistoryTypes(it).toFloat(), it))
+
         }
+
+        val barDataSet = PieDataSet(addedStatistics, "")
+        val colors = resources.getIntArray(R.array.rainbow).toMutableList()
+        barDataSet.colors = colors
+        barDataSet.setDrawValues(false)
+        val barData = PieData(barDataSet)
+        val centerText = addedStatistics.count()
+            .toString() + " " + requireContext().getString(R.string.AddedProducts)
+        statistics_addedProducts_PieChart.data = barData
+        statistics_addedProducts_PieChart.invalidate()
+        statistics_addedProducts_PieChart.description.isEnabled = false
+        statistics_addedProducts_PieChart.setDrawEntryLabels(false)
+        statistics_addedProducts_PieChart.centerText = centerText
+        statistics_addedProducts_PieChart.setHoleColor(requireContext().getColor(R.color.colorBackground))
+        statistics_addedProducts_PieChart.setCenterTextColor(requireContext().getColor(R.color.colorText))
+        statistics_addedProducts_PieChart.isDrawHoleEnabled = true
+        val l: Legend = statistics_addedProducts_PieChart.legend
+        l.verticalAlignment = Legend.LegendVerticalAlignment.TOP
+        l.horizontalAlignment = Legend.LegendHorizontalAlignment.RIGHT
+        l.isWordWrapEnabled = true
+        l.orientation = Legend.LegendOrientation.VERTICAL
+        l.textSize = 14F
+        l.textColor = requireContext().getColor(R.color.colorText)
+        l.setDrawInside(false)
+    }
+
+
+    private fun countStringInHistoryTypes(string: String): Int {
+    return historyTypesAddedProducts.count{item -> item == string}
+
+
+}
+
+    private fun setUpDeletedPieChart(it: List<Statistic>) {
+        val eatenCount = it.count { statistic -> statistic.eaten  }
         val thrownAwayCount = it.count() - eatenCount
         val eatenStatistics = ArrayList<PieEntry>()
         eatenStatistics.add(PieEntry(eatenCount.toFloat(), getString(R.string.eaten)))
         eatenStatistics.add(PieEntry(thrownAwayCount.toFloat(), getString(R.string.thrownAway)))
         val pieDataSet = PieDataSet(eatenStatistics, "")
+        pieDataSet.setDrawValues(false)
         pieDataSet.setColors(
             intArrayOf(
                 R.color.colorFresh,
@@ -60,23 +110,23 @@ class StatisticsFragment : Fragment() {
         pieData.isHighlightEnabled = false
         pieData.setValueTextColor(requireContext().getColor(R.color.colorText))
         pieData.setValueTextSize(24f)
-        val l: Legend = statistics_PieChart.legend
+        val l: Legend = statistics_deletedProducts_PieChart.legend
         l.verticalAlignment = Legend.LegendVerticalAlignment.TOP
         l.horizontalAlignment = Legend.LegendHorizontalAlignment.RIGHT
         l.orientation = Legend.LegendOrientation.VERTICAL
         l.textSize = 14F
         l.textColor = requireContext().getColor(R.color.colorText)
         l.setDrawInside(false)
-        statistics_PieChart.data = pieData
-        statistics_PieChart.setUsePercentValues(true)
-        statistics_PieChart.setDrawEntryLabels(false)
-        statistics_PieChart.isDrawHoleEnabled = true
-        statistics_PieChart.centerText =centerText
-        statistics_PieChart.setCenterTextSize(28f)
-        statistics_PieChart.setCenterTextColor(requireContext().getColor(R.color.colorText))
-        statistics_PieChart.setHoleColor(requireContext().getColor(R.color.colorBackground))
-        statistics_PieChart.invalidate()
-        statistics_PieChart.description.isEnabled = false
-        statistics_PieChart.animate()
+        statistics_deletedProducts_PieChart.data = pieData
+        statistics_deletedProducts_PieChart.setUsePercentValues(true)
+        statistics_deletedProducts_PieChart.setDrawEntryLabels(false)
+        statistics_deletedProducts_PieChart.isDrawHoleEnabled = true
+        statistics_deletedProducts_PieChart.centerText =centerText
+        statistics_deletedProducts_PieChart.setCenterTextSize(28f)
+        statistics_deletedProducts_PieChart.setCenterTextColor(requireContext().getColor(R.color.colorText))
+        statistics_deletedProducts_PieChart.setHoleColor(requireContext().getColor(R.color.colorBackground))
+        statistics_deletedProducts_PieChart.invalidate()
+        statistics_deletedProducts_PieChart.description.isEnabled = false
+        statistics_deletedProducts_PieChart.animate()
     }
 }
